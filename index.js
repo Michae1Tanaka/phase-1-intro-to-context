127a2ab9ea8d7b2e64ff6e991b68118469f2ae20
Michael Tanaka <tanakam4214@gmail.com>
2023-04-27 16:45:28 -0700

    Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,74 @@
-// Your code here
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+  const employee = {
+    firstName: firstName,
+    familyName: familyName,
+    title: title,
+    payPerHour: payPerHour,
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
+  return employee;
+}
+
+function createEmployeeRecords(multipleEmployeeArr) {
+  return multipleEmployeeArr.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(employeeObj, dateStamp) {
+  const [date, hour] = dateStamp.split(" ");
+  const timeInObj = {
+    type: "TimeIn",
+    hour: parseInt(hour),
+    date: date,
+  };
+  employeeObj.timeInEvents.push(timeInObj);
+  return employeeObj;
+}
+
+function createTimeOutEvent(employeeObj, dateStamp) {
+  const [date, hour] = dateStamp.split(" ");
+  const timeOutObj = {
+    type: "TimeOut",
+    hour: parseInt(hour),
+    date: date,
+  };
+  employeeObj.timeOutEvents.push(timeOutObj);
+  return employeeObj;
+}
+
+function hoursWorkedOnDate(employeeObj, soughtDate) {
+  let hoursWorked = 0;
+  const timeIn = employeeObj.timeInEvents.find(
+    (timeIn) => soughtDate === timeIn.date
+  );
+  const timeOut = employeeObj.timeOutEvents.find(
+    (timeIn) => soughtDate === timeIn.date
+  );
+  if (timeIn && timeOut) {
+    hoursWorked = (timeOut.hour - timeIn.hour) / 100;
+  }
+  return hoursWorked;
+}
+
+function wagesEarnedOnDate(employeeObj, soughtDate) {
+  const hoursWorked = hoursWorkedOnDate(employeeObj, soughtDate);
+  const payRate = employeeObj.payPerHour;
+  return hoursWorked * payRate;
+}
+
+function allWagesFor(employeeObj) {
+  console.log(employeeObj);
+  const datesWorked = employeeObj.timeInEvents.map((timeIn) => timeIn.date);
+  const totalWages = datesWorked.reduce(
+    (acc, date) => acc + wagesEarnedOnDate(employeeObj, date),
+    0
+  );
+  return totalWages;
+}
+
+function calculatePayroll(arrayOfEmployees) {
+  let totalWages = arrayOfEmployees.reduce((acc, employeeObj) => {
+    return acc + allWagesFor(employeeObj);
+  }, 0);
+  return totalWages;
+}
